e237ad046ca00c5841a034c44da9430663252d55
Kusum Choudhary <kusumchoudhary7020@gmail.com>
2024-10-13 16:26:16 +0530

    Used the second Approach for the same problem statement

--- Factorial.java
+++ Factorial.java
@@ -12,10 +12,25 @@ public class Factorial {
             fact=fact*i;
         System.out.println("Factorial is: "+fact);
     }
+
+    //Using Recursion (2nd Approach)
+    public static int factorialOfNum(int num) {
+        //base case
+        if(num == 0) {
+            return 1;
+        }
+        int fnm1 = factorialOfNum(num-1);
+        int fn = num * fnm1;
+        return fn;
+    }
+
     public static void main(String[] args) {
         Factorial Fact = new Factorial();
         System.out.print("Enter a Number: ");
         Fact.set_data();
         Fact.fact();
+        
+        //2nd Approach (Output) Call the function and pass the number in it
+        System.out.println("The Factorial of the number is : "+ factorialOfNum(4));
     }
 }
